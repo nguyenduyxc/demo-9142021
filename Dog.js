49b2ed9c5c815d8c2aa0d41c30fc8b0eb18ef32e
duy-git <dduy961@gmail.com>
2021-09-13 23:26:54 +0700

    test .gitignore

--- Dog.js
+++ Dog.js
@@ -1,3 +1,10 @@
-function dog(){
-   
+var chalk = require('chalk');
+function Dog(name){
+   this.name=name;
+ //  this.stomach =[]; 
+};
+
+Dog.prototype.sayHi=function(){
+    console.log('hi! my name is ' +chalk.blue(this.name));
 }
+module.exports = Dog;
